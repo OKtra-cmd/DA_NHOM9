18c9f9d50cb971a2cd0063395a86b050cd411c6a
OKtra-cmd <tamphatlam@gmail.com>
2025-11-19 16:25:59 +0700

    Ban ma hoa va giai ma Ceasar

---
+++ DemoCeasar.cs/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace DemoCeasar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.lstResults = new System.Windows.Forms.ListBox();
+            this.lblBest = new System.Windows.Forms.Label();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnLuu = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.btnHuongDan = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnBrute = new System.Windows.Forms.Button();
+            this.btnNapTuDien = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(101, 17);
+            this.txtInput.Margin = new System.Windows.Forms.Padding(2);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(97, 20);
+            this.txtInput.TabIndex = 0;
+            // 
+            // lstResults
+            // 
+            this.lstResults.FormattingEnabled = true;
+            this.lstResults.Location = new System.Drawing.Point(226, 107);
+            this.lstResults.Margin = new System.Windows.Forms.Padding(2);
+            this.lstResults.Name = "lstResults";
+            this.lstResults.Size = new System.Drawing.Size(314, 160);
+            this.lstResults.TabIndex = 2;
+            // 
+            // lblBest
+            // 
+            this.lblBest.AutoSize = true;
+            this.lblBest.Location = new System.Drawing.Point(20, 218);
+            this.lblBest.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblBest.Name = "lblBest";
+            this.lblBest.Size = new System.Drawing.Size(150, 13);
+            this.lblBest.TabIndex = 3;
+            this.lblBest.Text = "hiển thị kết quả rõ đúng nhất: ";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(101, 50);
+            this.txtOutput.Margin = new System.Windows.Forms.Padding(2);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(97, 20);
+            this.txtOutput.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtOutput);
+            this.groupBox1.Controls.Add(this.btnBrute);
+            this.groupBox1.Controls.Add(this.txtInput);
+            this.groupBox1.Controls.Add(this.btnNapTuDien);
+            this.groupBox1.Location = new System.Drawing.Point(12, 117);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(203, 80);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Điều Khiển";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(161, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(379, 30);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Ứng dụng mã hóa Cipher Brute-force";
+            // 
+            // btnLuu
+            // 
+            this.btnLuu.Location = new System.Drawing.Point(23, 244);
+            this.btnLuu.Name = "btnLuu";
+            this.btnLuu.Size = new System.Drawing.Size(75, 23);
+            this.btnLuu.TabIndex = 9;
+            this.btnLuu.Text = "Lưu";
+            this.btnLuu.UseVisualStyleBackColor = true;
+            this.btnLuu.Click += new System.EventHandler(this.btnLuu_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(119, 244);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(75, 23);
+            this.btnXoa.TabIndex = 10;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Image = global::DemoCeasar.Properties.Resources.exit_19;
+            this.btnThoat.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnThoat.Location = new System.Drawing.Point(465, 282);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(61, 23);
+            this.btnThoat.TabIndex = 12;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // btnHuongDan
+            // 
+            this.btnHuongDan.Image = global::DemoCeasar.Properties.Resources.instructions_19;
+            this.btnHuongDan.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnHuongDan.Location = new System.Drawing.Point(57, 282);
+            this.btnHuongDan.Name = "btnHuongDan";
+            this.btnHuongDan.Size = new System.Drawing.Size(92, 23);
+            this.btnHuongDan.TabIndex = 11;
+            this.btnHuongDan.Text = "Hướng Dẫn";
+            this.btnHuongDan.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnHuongDan.UseVisualStyleBackColor = true;
+            this.btnHuongDan.Click += new System.EventHandler(this.btnHuongDan_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::DemoCeasar.Properties.Resources.Logo_STU;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(137, 89);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnBrute
+            // 
+            this.btnBrute.Image = global::DemoCeasar.Properties.Resources.hammer_19;
+            this.btnBrute.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnBrute.Location = new System.Drawing.Point(11, 50);
+            this.btnBrute.Margin = new System.Windows.Forms.Padding(2);
+            this.btnBrute.Name = "btnBrute";
+            this.btnBrute.Size = new System.Drawing.Size(86, 20);
+            this.btnBrute.TabIndex = 5;
+            this.btnBrute.Text = "Brute-force";
+            this.btnBrute.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnBrute.UseVisualStyleBackColor = true;
+            this.btnBrute.Click += new System.EventHandler(this.btnBrute_Click);
+            // 
+            // btnNapTuDien
+            // 
+            this.btnNapTuDien.Image = global::DemoCeasar.Properties.Resources.dictionary_19;
+            this.btnNapTuDien.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnNapTuDien.Location = new System.Drawing.Point(11, 16);
+            this.btnNapTuDien.Margin = new System.Windows.Forms.Padding(2);
+            this.btnNapTuDien.Name = "btnNapTuDien";
+            this.btnNapTuDien.Size = new System.Drawing.Size(86, 20);
+            this.btnNapTuDien.TabIndex = 1;
+            this.btnNapTuDien.Text = "Tải từ điển";
+            this.btnNapTuDien.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnNapTuDien.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(573, 331);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnHuongDan);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnLuu);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblBest);
+            this.Controls.Add(this.lstResults);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Ứng dụng mã hóa Cipher Brute-force";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnNapTuDien;
+        private System.Windows.Forms.ListBox lstResults;
+        private System.Windows.Forms.Label lblBest;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnBrute;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnLuu;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnHuongDan;
+        private System.Windows.Forms.Button btnThoat;
+    }
+}
+
